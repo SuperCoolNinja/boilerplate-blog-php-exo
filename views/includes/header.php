a5381ae22899f2a0af5e33befb055cb80ad0d036
Super Cool Ninja <samir932205@gmail.com>
2022-07-04 13:42:59 +0200

    cleanup

--- views/includes/header.php
+++ views/includes/header.php
@@ -1,8 +1,4 @@
 <header class="background-header d-flex justify-content-center align-items-center">
-    <style><?php include 'main.css'; ?></style>
+    <style><?php include 'main.css'?></style>
     <a href="?page=index"><h1>UnitBlog</h1></a>
-
-    <?php if(!empty($_COOKIE['user']))
-        echo '<a class="nav-link"  aria-disabled="false" href="?action=checkIsLogged">Post Blog</a>';
-    else echo '<a class="nav-link"  aria-disabled="false" href="?page=login">Log in</a>';?>
 </header>
